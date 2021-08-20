4684f36587c0ca8aebcac84888ab4834c4f918bf
Lee Anderson <ecllips07@gmail.com>
2021-08-20 11:24:36 -0600

    gitlens

--- main.rb
+++ main.rb
@@ -18,3 +18,16 @@ puts add(2, 2)
 def minus(num1, num2)
     num1 - num2
 end
+
+# if there is not a repo
+# git init once to initialze the repo (rails does this for us)
+
+# once you start a project its good habit to do your init commit
+# git add .
+# git commit -m'put commit here'
+
+# start working on features
+# pick a chunck of work to do, get it working
+# git add . or (files you want to commit)
+# git commit -m'describe the work you did'
+# repeat this untill you are ready to push to github
