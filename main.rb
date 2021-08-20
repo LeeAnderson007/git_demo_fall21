bb0b5ccb07d8a991de412c00e96cfd79eb8acc80
Lee Anderson <ecllips07@gmail.com>
2021-08-20 11:00:41 -0600

    added add function

---
+++ main.rb
@@ -0,0 +1,16 @@
+puts "hello"
+
+
+# add: stages changes gets them to be comitted
+# git add .  to stage all changes
+# get add filename to stage individual changers
+
+# git commit -m  'describe what you did since you last commited'
+
+## creat a function that adds two numbers
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+puts add(2, 2)
